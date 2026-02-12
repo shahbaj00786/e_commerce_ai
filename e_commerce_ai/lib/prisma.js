0adaad3dbc7324f97876b7986101a723f1935eb8
Your Name <shahbajhussain@gmail.com>
2026-02-12 10:11:55 +0530

    "Neon table created"

---
+++ e_commerce_ai/lib/prisma.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+neonConfig.poolQueryViaFetch = true;
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaNeon({ connectionString });
+
+const globalForPrisma = global;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
